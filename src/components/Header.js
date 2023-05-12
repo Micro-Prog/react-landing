0818a401573d60b87b7c0c00a7a9b1cb305109f2
Micro-Prog <sajjadsttr01@gmail.com>
2023-05-12 21:06:17 +0330

    update

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <h1>A Movie River</h1>
+            </div>
+            <nav className="nav">
+                <ul>
+                    <li><a href="/">Movies</a></li>
+                    <li><a href="/">Category</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Contact Us</a></li>
+                </ul>
+            </nav>
+            <div className="btn">
+                <button type="button">Login</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="col col1">
+                <h2>Your Movies</h2>
+                <p>Click On the Title to Explore the Movie!</p>
+                <button type="button">More~</button>
+                </div>
+            <div className="col col2">
+                <div className="card card1"></div>
+                <div className="card card2"></div>
+                <div className="card card3"></div>
+                <div className="card card4"></div>
+                <div className="card card5"></div>
+                <div className="card card6"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
